db16b8d03d8d3a6f24063d97e6c02f11e429818c
Eliam <bosc790@gmail.com>
2023-03-09 13:36:19 -0800

    V0.2.0

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
 import data from "../Data/Periodic-Table-JSON.json"
 
+const colorMap = {
+  "noble gas": "#FFBC42",
+  "alkaline earth metal": "#EC674E",
+  "diatomic nonmetal": "#D81159",
+  "alkali metal": "#8F2D56",
+  "transition metal": "#58586B",
+  "post-transition metal": "#218380",
+  lanthanide: "#4AABAF",
+  metalloid: "#73D2DE"
+};
+
 const Table = () => {
   return (
     <>
         <div className='table-container'>
             {data.elements.map((element) => (
-                <div
-                    className='element'
-                    key={element.name} 
-                >
-                <strong>{element.symbol}</strong>
-                </div>
+              <div
+                className='element'
+                key={element.name}
+                style={{
+                  gridColumn: element.xpos,
+                  gridRow: element.ypos,
+                  borderColor: colorMap[element.category],
+                  backgroundColor: colorMap[element.category],
+                }}
+              >
+              <strong>{element.symbol}</strong>
+              </div>
             ))}
         </div>
     </>
